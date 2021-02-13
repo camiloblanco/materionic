ff2eb824b1e23c6c23f4c8473dd3755b8160731e
camiloBlanco <mail@camiloblanco.com>
2021-02-13 14:34:10 +0000

    first commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+import { HomePage } from './home.page';
+//import the material module
+import { MaterialModule } from '../material.module';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    MaterialModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
